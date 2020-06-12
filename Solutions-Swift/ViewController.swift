ad61a7731fa23623f76ee773b74c70de19f19287
Aman Ilyasovich <aman.ilyasovich@gmail.com>
2020-06-12 01:26:49 -0700

    Initial Commit

---
+++ Solutions-Swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Solutions-Swift
+//
+//  Created by Aman Ilyasovich on 6/12/20.
+//  Copyright © 2020 Aman Ilyasovich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
